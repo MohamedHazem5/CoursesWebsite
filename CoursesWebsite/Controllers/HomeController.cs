5db2c496d2b2ee74028a0531b3aa4e7e592fb5d0
Mohamed Hazem <100438594+MohamedHazem5@users.noreply.github.com>
2023-02-03 00:00:04 +0200

    New

--- CoursesWebsite/Controllers/HomeController.cs
+++ CoursesWebsite/Controllers/HomeController.cs
@@ -18,10 +18,7 @@ namespace CoursesWebsite.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
