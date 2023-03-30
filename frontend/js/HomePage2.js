5ef78629a8e3e6f6404783ee75f621f4347678c3
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-03-30 09:26:58 +0200

    counter up area

--- frontend/js/HomePage2.js
+++ frontend/js/HomePage2.js
@@ -22,14 +22,14 @@ back.addEventListener("click", ()=>
  * animate meet our experts section 
  * all credits to https://animate.style/
  */
-var animate_elements = document.getElementsByClassName("animate__animated");
-
+var animate_elements = document.getElementsByClassName("animate__animated card");
 let reached_0 = setInterval(()=>
 {
     if(animate_elements[0].getBoundingClientRect()["top"] <= (window.innerHeight - 30))
     {
         animate_elements[0].classList.add("animate__fadeInLeft");
         animate_elements[0].classList.add("animate__delay-0.75s"); 
+        clearInterval(reached_0);
     }
 }, 200)
 let reached_1 = setInterval(()=>
@@ -38,6 +38,7 @@ let reached_1 = setInterval(()=>
     {
         animate_elements[1].classList.add("animate__fadeInUp");
         animate_elements[1].classList.add("animate__delay-0.75s");
+        clearInterval(reached_1);
     }
 }, 200)
 let reached_2 = setInterval(()=>
@@ -46,6 +47,7 @@ let reached_2 = setInterval(()=>
     {
         animate_elements[2].classList.add("animate__fadeInUp");
         animate_elements[2].classList.add("animate__delay-0.75s");
+        clearInterval(reached_2);
     }
 }, 200)
 let reached_3 = setInterval(()=>
@@ -54,5 +56,58 @@ let reached_3 = setInterval(()=>
     {
         animate_elements[3].classList.add("animate__fadeInRight");
         animate_elements[3].classList.add("animate__delay-0.75s");
+        clearInterval(reached_3);
     }
 }, 200)
+/* end of animation meet our experts*/
+/**
+ * counter up
+ */
+var itr1 = 0;
+let icon_1 = setInterval(()=>
+{
+    var icon1 = document.getElementById("Icon1");
+    if(icon1.getBoundingClientRect()["top"] <= (window.innerHeight - 30))
+    {
+        icon1.innerHTML = `${itr1}`;
+        itr1+=11;
+        if (itr1 >= 5670)
+            clearInterval(icon_1);
+    }
+}, 1)
+var itr2 = 0;
+let icon_2 = setInterval(()=>
+{
+    var icon2 = document.getElementById("Icon2");
+    if(icon2.getBoundingClientRect()["top"] <= (window.innerHeight - 30))
+    {
+        icon2.innerHTML = `${itr2}`;
+        itr2 += 11;
+        if (itr2 >= 5400)
+            clearInterval(icon_2);
+    }
+}, 1)
+var itr3 = 0;
+let icon_3 = setInterval(()=>
+{
+    var icon3 = document.getElementById("Icon3");
+    if(icon3.getBoundingClientRect()["top"] <= (window.innerHeight - 30))
+    {
+        icon3.innerHTML = `${itr3}`;
+        itr3 += 4;
+        if (itr3 >= 760)
+            clearInterval(icon_3);
+    }
+}, 9)
+var itr4 = 0;
+let icon_4 = setInterval(()=>
+{
+    var icon4 = document.getElementById("Icon4");
+    if(icon4.getBoundingClientRect()["top"] <= (window.innerHeight - 30))
+    {
+        icon4.innerHTML = `${itr4}`;
+        itr4 += 5;
+        if (itr4 >= 2460)
+            clearInterval(icon_4);
+    }
+}, 1)
